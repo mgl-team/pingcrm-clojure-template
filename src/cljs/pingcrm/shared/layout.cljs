6dd9fc50c0b6a26e67f7e899b53c04eb80bbd93c
amjil <amjil@protonmail.com>
2021-10-14 15:26:51 +0800

    menu rotate 45 degree to reduce top header height

--- src/cljs/pingcrm/shared/layout.cljs
+++ src/cljs/pingcrm/shared/layout.cljs
@@ -12,12 +12,12 @@
    [:div {:class "md:flex md:flex-col"}
     [:div {:class "md:h-screen md:flex md:flex-row"}
      [:div {:class "md:flex md:flex-col md:flex-shrink-0"}
-      [:div {:class "bg-indigo-900 md:flex-shrink-0 md:h-56 px-2 py-4 flex items-center justify-between md:justify-center"}
+      [:div {:class "bg-indigo-900 md:flex-shrink-0 md:h-36 px-2 py-4 flex items-center justify-between md:justify-center"}
        [:> InertiaLink {:class "", :href "/"}
         [logo {:class "fill-white transform rotate-90", :height "120", :width "28"}]]]
       [:f> bottom-header]]
      [:div {:class "flex flex-row flex-grow overflow-hidden mode-lr"}
-      [:f> main-menu {:class "flex-shrink-0 hidden h-56 p-12 overflow-x-auto bg-indigo-800 md:block"}]
+      [:f> main-menu {:class "flex-shrink-0 hidden h-36 pl-32 pt-3 overflow-x-auto bg-indigo-800 md:block"}]
       ;; To reset scroll region (https://inertiajs.com/pages#scroll-regions) add
       ;; `scroll-region="true"` to div below
       [:div {:class "h-full py-4 px-8 overflow-hidden overflow-y-auto md:p-12"}
--- src/cljs/pingcrm/shared/menu_item.cljs
+++ src/cljs/pingcrm/shared/menu_item.cljs
@@ -7,7 +7,7 @@
   (let [active? (or (and (= current-url "") (= link "dashboard")) (str/starts-with? current-url link))
         icon-class (str "h-4 w-4 mb-2 " (if active? "text-white fill-current" "text-indigo-400 group-hover:text-white fill-current"))
         text-class (if active? "text-white" "text-indigo-300 group-hover:text-white")]
-    [:div {:class "mr-4"}
+    [:div {:class "mr-4 origin-left transform -rotate-45"}
      [:> InertiaLink {:href (js/route link) :class "flex items-center group px-3"}
       [icon {:name icon-name :class icon-class}]
       [:div {:class text-class}
