6e56f4c5e8d70c7f31cebfb6b3161453c608b5b0
amjil <amjil@protonmail.com>
2021-10-15 15:31:30 +0800

    login page

--- src/cljs/pingcrm/pages/login.cljs
+++ src/cljs/pingcrm/pages/login.cljs
@@ -7,7 +7,7 @@
 
 (defn login-form []
   (let [{:keys [data setData errors post processing]} (j/lookup
-                                                       (useForm #js {:email "johndoe@example.com"
+                                                       (useForm #js {:email "SaveTheMongolian@example.com"
                                                                      :password "secret"
                                                                      :remember false}))
         on-submit #(do (.preventDefault %)
@@ -18,34 +18,34 @@
       [:div {:class "w-full max-w-md"}
        [logo {:class "block mx-auto w-full max-w-xs fill-white" :height "50"}]
        [:form {:on-submit on-submit
-               :class "mt-8 overflow-hidden bg-white rounded-lg shadow-xl"}
-        [:div {:class "px-10 py-12"}
+               :class "mt-8 overflow-hidden bg-white rounded-lg shadow-xl mode-lr"}
+        [:div {:class "py-10 px-12"}
          [:h1 {:class "text-3xl font-bold text-center"} "Welcome Back!"]
-         [:div {:class "w-24 mx-auto mt-6 border-b-2"}]
-         [text-input {:class "mt-10"
+         [:div {:class "h-24 mx-auto mr-6 border-l-2"}]
+         [text-input {:class "mr-10"
                       :label "Email"
                       :name "email"
                       :errors (.-email errors)
                       :value (.-email data)
                       :on-change #(setData "email" (.. % -target -value))}]
-         [text-input {:class "mt-6"
+         [text-input {:class "mr-6"
                       :label "Password"
                       :name "password"
                       :type "password"
                       :errors (.-password errors)
                       :value (.-password data)
                       :on-change #(setData "password" (.. % -target -value))}]
-         [:label {:class "flex items-center mt-6 select-none" :html-for "remember"}
+         [:label {:class "flex items-center mr-6 select-none" :html-for "remember"}
           [:input#remember
            {:name "remember"
-            :class "mr-1"
+            :class "mb-1"
             :type "checkbox"
             :checked (.-remember data)
             :on-change #(setData "remember" (.. % -target -checked))}]
           [:span {:class "text-sm"} "Remember Me"]]]
-        [:div {:class "px-10 py-4 bg-gray-100 border-t border-gray-100 flex"}
+        [:div {:class "py-10 px-4 bg-gray-100 border-r border-gray-100 flex"}
          [loading-button
-          {:type "submit" :loading processing :class "btn-indigo  ml-auto"}
+          {:type "submit" :loading processing :class "btn-indigo  mt-auto"}
           "Login"]]]]]]))
 
 (defn login []
